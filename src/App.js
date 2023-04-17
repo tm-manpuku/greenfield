d10b712dbfdf0d18156997d6c0169efc2d4f82c6
dFumiaki <fumiaki.andou.fb@nttdocomo.com>
2023-04-17 10:57:44 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState } from "react";
+import { BrowserRouter, Link, Routes, Route} from "react-router-dom";
+import { App2 } from './App2';
+
+function App() {
+  const [string, setString] = useState('');
+  const [string2, setString2] = useState('');
+
+  return (
+ 
+    <div className="App">
+      <p>隠れ名店.com</p>
+      <input placeholder="場所を入力" onChange={(e) => {setString(e.target.value)}}>
+      </input>
+      <input placeholder="ジャンルを入力" onChange={(e) => {setString2(e.target.value)}}>
+      </input>
+      <button className="search" >検索</button>
+     {/* <BrowserRouter>
+     <Link to="/main">メインページへ</Link>
+      <Routes>
+          <Route path="/main">
+            <App2 />
+          </Route>
+      </Routes>
+    </BrowserRouter> */}
+    </div>
+
+  );
+}
+
+export default App;
---
+++ src/App2.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+export const App2 = () => {
+
+  return (
+ 
+    <div className="App">
+      <p>遷移後</p>
+    </div>
+
+  );
+}
+
+export default App2;
