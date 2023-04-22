6da89ffc72fd1a6f39ccb5446b3d963364d00f2c
dFumiaki <fumiaki.andou.fb@nttdocomo.com>
2023-04-23 02:29:40 +0900

    merge ohishi change

--- src/App.js
+++ src/App.js
@@ -1,35 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
-import { BrowserRouter, Route, Routes} from "react-router-dom";
-import { Main } from './Main';
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Main } from "./Main";
 import { Top } from "./Top";
-import { useState } from 'react';
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
-import Container from '@mui/material/Container';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import Tooltip from '@mui/material/Tooltip';
-import MenuItem from '@mui/material/MenuItem';
-import AdbIcon from '@mui/icons-material/Adb';
-import theme from './ColorTheme';
-import { ThemeProvider } from '@mui/material';
-import {CssBaseline} from '@mui/material';
+import { TopChoice } from "./TopChoice.js";
+import { useState } from "react";
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import Tooltip from "@mui/material/Tooltip";
+import MenuItem from "@mui/material/MenuItem";
+import AdbIcon from "@mui/icons-material/Adb";
+import theme from "./ColorTheme";
+import { ThemeProvider } from "@mui/material";
+import { CssBaseline } from "@mui/material";
 
-const pages = ['Home', 'Shop', 'History'];
-const settings = ['Profile', 'Logout'];
-
+const pages = ["Home", "Shop", "History"];
+const settings = ["Profile", "Logout"];
 
 function App() {
   const [anchorElNav, setAnchorElNav] = React.useState(null);
   const [anchorElUser, setAnchorElUser] = React.useState(null);
-  const [shopData,setShopData] = useState([]);
+  const [shopData, setShopData] = useState([]);
 
   const handleOpenNavMenu = (event) => {
     setAnchorElNav(event.currentTarget);
@@ -48,138 +48,142 @@ function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <CssBaseline/>
-    <>
-    <AppBar position="static">
-      <Container maxWidth="xl">
-        <Toolbar disableGutters>
-          <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
-          <Typography
-            variant="h6"
-            noWrap
-            component="a"
-            href="/"
-            sx={{
-              mr: 2,
-              display: { xs: 'none', md: 'flex' },
-              fontFamily: 'monospace',
-              fontWeight: 700,
-              letterSpacing: '.3rem',
-              color: 'inherit',
-              textDecoration: 'none',
-            }}
-          >
-            ランチ開拓.com
-          </Typography>
-
-          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
-            >
-              <MenuIcon />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'left',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'left',
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{
-                display: { xs: 'block', md: 'none' },
-              }}
-            >
-              {pages.map((page) => (
-                <MenuItem key={page} onClick={handleCloseNavMenu}>
-                  <Typography textAlign="center">{page}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
-          <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
-          <Typography
-            variant="h5"
-            noWrap
-            component="a"
-            href=""
-            sx={{
-              mr: 2,
-              display: { xs: 'flex', md: 'none' },
-              flexGrow: 1,
-              fontFamily: 'monospace',
-              fontWeight: 700,
-              letterSpacing: '.3rem',
-              color: 'inherit',
-              textDecoration: 'none',
-            }}
-          >
-            ランチ開拓.com
-          </Typography>
-          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-            {pages.map((page) => (
-              <Button
-                key={page}
-                onClick={handleCloseNavMenu}
-                sx={{ my: 2, color: 'white', display: 'block' }}
+      <CssBaseline />
+      <>
+        <AppBar position="static">
+          <Container maxWidth="xl">
+            <Toolbar disableGutters>
+              <AdbIcon sx={{ display: { xs: "none", md: "flex" }, mr: 1 }} />
+              <Typography
+                variant="h6"
+                noWrap
+                component="a"
+                href="/"
+                sx={{
+                  mr: 2,
+                  display: { xs: "none", md: "flex" },
+                  fontFamily: "monospace",
+                  fontWeight: 700,
+                  letterSpacing: ".3rem",
+                  color: "inherit",
+                  textDecoration: "none",
+                }}
               >
-                {page}
-              </Button>
-            ))}
-          </Box>
+                ランチ開拓.com
+              </Typography>
 
-          <Box sx={{ flexGrow: 0 }}>
-            <Tooltip title="Open settings">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
-              </IconButton>
-            </Tooltip>
-            <Menu
-              sx={{ mt: '45px' }}
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
-            >
-              {settings.map((setting) => (
-                <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                  <Typography textAlign="center">{setting}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
-        </Toolbar>
-      </Container>
-    </AppBar>
+              <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+                <IconButton
+                  size="large"
+                  aria-label="account of current user"
+                  aria-controls="menu-appbar"
+                  aria-haspopup="true"
+                  onClick={handleOpenNavMenu}
+                  color="inherit"
+                >
+                  <MenuIcon />
+                </IconButton>
+                <Menu
+                  id="menu-appbar"
+                  anchorEl={anchorElNav}
+                  anchorOrigin={{
+                    vertical: "bottom",
+                    horizontal: "left",
+                  }}
+                  keepMounted
+                  transformOrigin={{
+                    vertical: "top",
+                    horizontal: "left",
+                  }}
+                  open={Boolean(anchorElNav)}
+                  onClose={handleCloseNavMenu}
+                  sx={{
+                    display: { xs: "block", md: "none" },
+                  }}
+                >
+                  {pages.map((page) => (
+                    <MenuItem key={page} onClick={handleCloseNavMenu}>
+                      <Typography textAlign="center">{page}</Typography>
+                    </MenuItem>
+                  ))}
+                </Menu>
+              </Box>
+              <AdbIcon sx={{ display: { xs: "flex", md: "none" }, mr: 1 }} />
+              <Typography
+                variant="h5"
+                noWrap
+                component="a"
+                href=""
+                sx={{
+                  mr: 2,
+                  display: { xs: "flex", md: "none" },
+                  flexGrow: 1,
+                  fontFamily: "monospace",
+                  fontWeight: 700,
+                  letterSpacing: ".3rem",
+                  color: "inherit",
+                  textDecoration: "none",
+                }}
+              >
+                ランチ開拓.com
+              </Typography>
+              <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+                {pages.map((page) => (
+                  <Button
+                    key={page}
+                    onClick={handleCloseNavMenu}
+                    sx={{ my: 2, color: "white", display: "block" }}
+                  >
+                    {page}
+                  </Button>
+                ))}
+              </Box>
 
-            <BrowserRouter>
-            <Routes>
-            <Route path="/" element={<Top setShopData={setShopData}/>} />
-            <Route path="main" element={<Main shopData={shopData}/>} />
-            </Routes>
-            </BrowserRouter>
-            </>
-            </ThemeProvider>
+              <Box sx={{ flexGrow: 0 }}>
+                <Tooltip title="Open settings">
+                  <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                    <Avatar
+                      alt="Remy Sharp"
+                      src="/static/images/avatar/2.jpg"
+                    />
+                  </IconButton>
+                </Tooltip>
+                <Menu
+                  sx={{ mt: "45px" }}
+                  id="menu-appbar"
+                  anchorEl={anchorElUser}
+                  anchorOrigin={{
+                    vertical: "top",
+                    horizontal: "right",
+                  }}
+                  keepMounted
+                  transformOrigin={{
+                    vertical: "top",
+                    horizontal: "right",
+                  }}
+                  open={Boolean(anchorElUser)}
+                  onClose={handleCloseUserMenu}
+                >
+                  {settings.map((setting) => (
+                    <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                      <Typography textAlign="center">{setting}</Typography>
+                    </MenuItem>
+                  ))}
+                </Menu>
+              </Box>
+            </Toolbar>
+          </Container>
+        </AppBar>
+
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Top setShopData={setShopData} />} />
+            <Route path="choice" element={<TopChoice shopData={shopData} />} />
+            <Route path="main" element={<Main shopData={shopData} />} />
+          </Routes>
+        </BrowserRouter>
+      </>
+    </ThemeProvider>
   );
 }
 export default App;
--- src/Top.js
+++ src/Top.js
@@ -1,63 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { Button ,Grid} from "@mui/material"; 
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
-import Typography from '@mui/material/Typography';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import Card from '@mui/material/Card';
-import { LoadingButton } from '@mui/lab';
-import CssBaseline from '@mui/material/CssBaseline';
-import Container from '@mui/material/Container';
-import Paper from '@mui/material/Paper';
+import { Button, Grid } from "@mui/material";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import Typography from "@mui/material/Typography";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Card from "@mui/material/Card";
+import { LoadingButton } from "@mui/lab";
+import CssBaseline from "@mui/material/CssBaseline";
+import Container from "@mui/material/Container";
+import Paper from "@mui/material/Paper";
 import { getShop } from "./utils/get";
-import { CircularProgress } from '@mui/material';
+import { CircularProgress } from "@mui/material";
 
 export const Top = (props) => {
   const navigate = useNavigate();
-  const [locationState, setLocation] = useState('');
-  const [genreState, setGenre] = useState('');
-  const initialURL = "https://rugmhzne06.execute-api.us-east-1.amazonaws.com/prod?query=" + locationState +"+"+ genreState;
-  const limitedURL = "https://q8ivytu0o8.execute-api.us-east-1.amazonaws.com/prod?query=" + locationState +"+"+ genreState;
+  const [locationState, setLocation] = useState("");
+  const [genreState, setGenre] = useState("");
+  const initialURL =
+    "https://rugmhzne06.execute-api.us-east-1.amazonaws.com/prod?query=" +
+    locationState +
+    "+" +
+    genreState;
+  const limitedURL =
+    "https://q8ivytu0o8.execute-api.us-east-1.amazonaws.com/prod?query=" +
+    locationState +
+    "+" +
+    genreState;
   const [isLoading, setIsLoading] = useState(false);
-  const getShopLists = async () => {
-    try {
-      setIsLoading(true);
-      const getShopData = await fetch(limitedURL);
-      const resultOfShopData = await getShopData.json();
-      //const getPhotoData = await fetch(resultOfShopData[0].photo);
-      //console.log(getPhotoData);
-      props.setShopData(resultOfShopData);
-      setIsLoading(false);
-    }catch(err){
-      console.log("検索に失敗しました");
-    }
-    
-  }
+  // const getShopLists = async () => {
+  //   try {
+  //     setIsLoading(true);
+  //     const getShopData = await fetch(limitedURL);
+  //     const resultOfShopData = await getShopData.json();
+  //     //const getPhotoData = await fetch(resultOfShopData[0].photo);
+  //     //console.log(getPhotoData);
+  //     props.setShopData(resultOfShopData);
+  //     setIsLoading(false);
+  //   } catch (err) {
+  //     console.log("検索に失敗しました");
+  //   }
+  // };
 
   return (
-    <Container component="main" maxWidth="xs">
-    <Paper variant="outlined" sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }}>
-    <CssBaseline />
-    <Box
-      sx={{
-        marginTop: 8,
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
+    <Container
+      component="main"
+      maxWidth="xs"
+      style={{
+        backgroundImage: `url(${"./topimage_fried-egg-with-guacamole-sandwiches.jpg"})`,
       }}
     >
-    
-      <Typography component="h1" variant="h5">
-         ねえ、ご飯屋さん開拓しない？
-      </Typography>
-       <Typography component="h1" variant="h5">
-         今日のランチ新しいお店を開拓しませんか？早く決めるとお得に開拓できるよ！
-      </Typography>
-        <TextField
+      <Paper
+        variant="outlined"
+        sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }}
+      >
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Typography component="h1" variant="h4">
+            ねえ、ご飯屋さん開拓しない？
+          </Typography>
+          <Typography component="h1" variant="h6">
+            今日のランチ新しいお店を開拓しませんか？早く決めるとお得に開拓できるよ！
+          </Typography>
+          {/* <TextField
           margin="normal"
           required
           fullWidth
@@ -76,31 +91,31 @@ export const Top = (props) => {
           label="ジャンル"
           type="ジャンル"
           onChange={(e) => {setGenre(e.target.value)}}
-        />
-      
-        <LoadingButton
-          size="medium"
-          variant="contained"
-          sx={{ mt: 3, mb: 2 }}
-          className="search"
-          loading={isLoading} 
-          onClick={
-          async() => {
-          await getShopLists();
-          navigate("/main",
-          {state:
-            {locationPara: {locationState},genrePara: {genreState}}
-          }
-          );
-        }}>
-          <span>検索</span>
+        /> */}
+
+          <LoadingButton
+            size="medium"
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+            className="search"
+            loading={isLoading}
+            onClick={() => {
+              navigate("/choice", {
+                state: {
+                  // locationPara: { locationState },
+                  // genrePara: { genreState },
+                },
+              });
+              // }
+            }}
+          >
+            <span>冒険してみる！</span>
           </LoadingButton>
-
         </Box>
-        </Paper>
-      </Container>
-
+      </Paper>
+    </Container>
   );
-}
+};
 
 export default Top;
+
